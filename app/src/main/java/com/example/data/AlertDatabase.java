3ec91ac9baa49d517b71a6fbe3802ddcaf1ba6ca
gideonotieno99 <gideonotieno99@gmail.com>
2025-10-28 08:27:29 +0000

    room database singleton

--- app/src/main/java/com/example/data/AlertDatabase.java
+++ app/src/main/java/com/example/data/AlertDatabase.java
@@ -1,4 +1,28 @@
 package com.example.data;
 
-public class AlertDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AlertEntity.class}, version = 1, exportSchema = false)
+public abstract class AlertDatabase extends RoomDatabase {
+
+    public abstract AlertDao alertDao();
+
+    private static volatile AlertDatabase INSTANCE;
+
+    public static AlertDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AlertDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AlertDatabase.class, "alert_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
