c9ea5b0d12564351d869a9216b963dcfd0169d40
gideonotieno <bruceotieno25@gmail.com>
2026-03-26 12:20:30 +0300

    added incoming SOS UI activity

--- app/src/main/java/com/example/core/EmergencyAlertActivity.java
+++ app/src/main/java/com/example/core/EmergencyAlertActivity.java
@@ -10,17 +10,34 @@ public class EmergencyAlertActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        android.location.Location location = getIntent().getParcelableExtra("LOCATION");
+        String senderName = getIntent().getStringExtra("sender_name");
+        String senderNumber = getIntent().getStringExtra("sender_number");
+        if (senderName == null) senderName = "Your contact";
+        if (senderNumber == null) senderNumber = "";
+        final String finalNumber = senderNumber;
+        //android.location.Location location = getIntent().getParcelableExtra("LOCATION");
 
-        EmergencyAlertDialog.show(this, new EmergencyAlertDialog.OnAlertActionListener() {
-            @Override
-            public void onAlertSent() {
-                finish();
-            }
-            @Override
-            public void onAlertCancelled() {
-                finish();
-            }
-        }, location);
+        EmergencyAlertDialog.showIncomingSOS(
+                this,
+                senderName,
+                finalNumber,
+                new EmergencyAlertDialog.OnAlertActionListener() {
+                    @Override
+                    public void onAlertSent() {
+                        finish();
+                    }
+
+                    @Override
+                    public void onAlertCancelled() {
+                        SilentSmsReceiver.stopAlarm();
+                        finish();
+                    }
+                }
+            );
+        }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SilentSmsReceiver.stopAlarm();
     }
 }
